1da76d704028e7a62b2c00faec258918333670bf
Patrick Kozma <patrick@12finger.com>
2017-09-11 15:08:38 +0200

    example

--- app/src/containers/App.jsx
+++ app/src/containers/App.jsx
@@ -6,9 +6,39 @@ export default class App extends Component {
     render() {
         return (
           <div className="app-container">
-            <Logo />
-            <Counter />             
+            <h2 id="heading">Hello Ioana</h2>
+
+            <div className="some-content">
+                <div className="child">
+                    Contents
+                </div>
+                <div className="child">
+                    more Contents
+                </div>
+                <div className="child">
+                    even more Contents
+                </div>
+            </div>
+
+
+            <div className="some-content">
+                <div className="child">
+                    Contents
+                </div>
+                <div className="child">
+                    more Contents
+                </div>
+                <div className="child">
+                    even more Contents
+                </div>
+            </div>
+
+
           </div>
         );
     }
 }
+
+
+// <Logo />
+// <Counter />
